545d8e2d111526a2b7fd624385a8ffdb3101b398
Putinjov <putinjov@gmail.com>
2023-04-14 17:35:23 +0300

    create getTrailer function

---
+++ src/js/get-trailer.js
@@ -0,0 +1,40 @@
+import basicLightbox from 'basiclightbox';
+import getMovieById from './get-movie-by-id';
+let keyTrailer = '';
+
+export async function showTrailer() {
+    const film = await getMovieById(id)
+    try {
+        if (movies && movies.length) {
+          const objTrailer = movies.find(movie => movie.type === 'Trailer');
+
+          if (objTrailer && objTrailer.type !== 'Trailer') {
+            return;
+          }
+          keyTrailer = objTrailer ? objTrailer.key : '';
+        }
+
+        refresh(data, id, keyTrailer);
+        keyTrailer = '';
+      }
+    catch{(error => {
+        refresh(data, id);
+      });
+  };
+}
+    
+  const trailerRef = document.querySelector('[data-btn=watchTrailer]');
+  if (!keyTrailer) {
+    trailerRef.classList.add('is-hidden');
+  }
+
+  trailerRef.onclick = () => {
+    basicLightbox
+      .create(
+        `<iframe width="640" height="360"
+        src="https://www.youtube.com/embed/${keyTrailer}"
+        title="" frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+      )
+      .show();
+  }
