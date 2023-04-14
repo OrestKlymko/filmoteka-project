9fb05bcd68ef43f89751ac0428f0657779edbed5
Ihor Karen <karenihor@gmail.com>
2023-04-14 19:49:46 +0300

    import - getMovieById

--- src/js/local-storage.js
+++ src/js/local-storage.js
@@ -1,14 +1,4 @@
-// Функція для запиту на бек, видалити в майбутньому і зробити імпорт
-const MAIN_URL = 'https://api.themoviedb.org/3';
-const API_KEY = 'ca3ef83497283bd11ad4f2544336ab4a';
-
-async function getInfoMovie(movie_id) {
-  const url = `${MAIN_URL}/movie/${movie_id}?api_key=${API_KEY}&language=en-US`;
-  return await fetch(url)
-    .then(response => response.json())
-    .catch(error => {});
-}
-////////////////////////////////////////////////////////////////////////////////////
+import getMovieById from './get-movie-by-id';
 
 let watchedMovies = JSON.parse(localStorage.getItem('watchedMovies')) || [];
 let queuedMovies = JSON.parse(localStorage.getItem('queuedMovies')) || [];
@@ -18,16 +8,16 @@ const addToQueueBtn = document.querySelector('.queueBtn');
 
 addToWatchedBtn.addEventListener('click', async e => {
   const movieId = 594767; // e.target.getAttribute('data-id');
-  const movieObj = await getInfoMovie(movieId);
+  const movieObj = await getMovieById(movieId);
 
-  //   Перевірка на наявність фільму в масиві 
+  //   Перевірка на наявність фільму в масиві
   const isMovieInWatched = watchedMovies.some(
     movie => movie.id === movieObj.id
   );
 
   if (isMovieInWatched) {
     watchedMovies = watchedMovies.filter(movie => movie.id !== movieObj.id); //Видалення, якщо фільм вже є в масиві
-  } else {  
+  } else {
     watchedMovies.push(movieObj); // Якщо немає, додаємо фільм в масив
   }
 
@@ -36,7 +26,7 @@ addToWatchedBtn.addEventListener('click', async e => {
 
 addToQueueBtn.addEventListener('click', async e => {
   const movieId = 594767; // e.target.getAttribute('data-id');
-  const movieObj = await getInfoMovie(movieId);
+  const movieObj = await getMovieById(movieId);
 
   //   Перевірка на наявність фільму в масиві
   const isMovieInQueue = queuedMovies.some(movie => movie.id === movieObj.id); //Видалення, якщо фільм вже є в масиві
