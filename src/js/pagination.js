c057b12560d2aeb2483d5d269933be344181e006
Anna Kravets <annakravets1906@gmail.com>
2023-04-14 22:38:36 +0300

    Update pagination.js

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,8 +1,13 @@
+'use strict';
+
+import { getPopularMovies } from './get-popular-movies';
 import Pagination from 'tui-pagination';
 
 const refs = {
   pageButton: document.querySelector('.tui-pagination'),
 };
+
+const getPopularMoviesAPI = new getPopularMovies();
 let pageCount = 1;
 
 refs.pageButton.addEventListener('click', onNextPage);
@@ -41,10 +46,11 @@ function pagination(el) {
   const pagination = new Pagination(container, options);
 }
 
-getPopularMovies()
+getPopularMoviesAPI
+  .fetchPopularMovies()
   .then(data => {
     markuplist(data);
-    refs.listEl.innerHTML = imagesList;
+    // refs.listEl.innerHTML = filmList;
     pagination(data);
   })
   .catch(error => {
@@ -52,6 +58,51 @@ getPopularMovies()
   });
 
 function onNextPage(event) {
-  pageCount = event.target.textContent;
-  // loadMore();
+  pageCount = Number(event.target.textContent);
+  console.log(pageCount);
+  pageButtonNumber();
+  // pageButtonNext();
+}
+
+function pagination(el) {
+  const container = document.getElementById('tui-pagination-container');
+  const options = {
+    totalItems: `${el.total_results}`,
+    itemsPerPage: `${el.results.length}`,
+    visiblePages: 5,
+    page: 1,
+    centerAlign: false,
+    firstItemClassName: 'tui-first-child',
+    lastItemClassName: 'tui-last-child',
+    template: {
+      page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+      currentPage:
+        '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+      moveButton:
+        '<a href="#" class="tui-page-btn tui-{{type}}">' +
+        '<span class="tui-ico-{{type}}">{{type}}</span>' +
+        '</a>',
+      disabledMoveButton:
+        '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+        '<span class="tui-ico-{{type}}">{{type}}</span>' +
+        '</span>',
+      moreButton:
+        '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+        '<span class="tui-ico-ellip">...</span>' +
+        '</a>',
+    },
+  };
+  const pagination = new Pagination(container, options);
+}
+
+function pageButtonNumber() {
+  getPopularMoviesAPI.page = pageCount;
+
+  if (Number.isInteger(getPopularMoviesAPI.page)) {
+    getPopularMoviesAPI.fetchPopularMovies().then(el => {
+      markuplist(el);
+
+      // refs.listEl.innerHTML = filmList;
+    });
+  }
 }
