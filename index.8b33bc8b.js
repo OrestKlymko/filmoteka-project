3ed4e8c5ecccdc8b18376c972a803b5135a55ae3
OrestKlymko <116631168+OrestKlymko@users.noreply.github.com>
2023-04-15 09:39:31 +0000

    Deploying to gh-pages from @ OrestKlymko/filmoteka-project@8dddcec923703d0227a4d254d55659bf11c9cb02 🚀

---
+++ index.8b33bc8b.js
@@ -0,0 +1,2 @@
+document.querySelector(".modal__btn-close").addEventListener("click",t),document.addEventListener("keydown",(e=>{"Escape"===e.key&&t()}));const e=document.querySelector(".modal");function t(){document.querySelector(".modal").classList.remove("modal--visible")}e.addEventListener("click",(c=>{c.target===e&&t()}));
+//# sourceMappingURL=index.8b33bc8b.js.map
