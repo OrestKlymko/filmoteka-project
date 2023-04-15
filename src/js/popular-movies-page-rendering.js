f6b1da51b069f9c0ac8e04724e973a7cae53bf1a
Oksana-Pelekhata <ok.soloviova@gmail.com>
2023-04-16 02:10:18 +0300

    popular-movies-page-rendering

--- src/js/popular-movies-page-rendering.js
+++ src/js/popular-movies-page-rendering.js
@@ -1,17 +1,49 @@
-// import { createMarkUp } from './search-movie-page-rendering'
+import { createMarkUp } from './search-movie-page-rendering'
 // import {getPopularMovies} from './get-popular-movies'
 
+//запит на бекенд, видалити після імпорту
+export class getPopularMovies {
+  BASE_URL = 'https://api.themoviedb.org/3';
+  API_KEY = '?api_key=f7d7a9b2e374f67b5381a74b61fb7dc2';
+  page = 1;
 
-// const popularMoviesPageRendering = async() => {
-//     try {
-//         const {results} = await getPopularMovies()
-//         console.log(results)
+  async fetchPopularMovies() {
+    try {
+      const response = await fetch(
+        `${this.BASE_URL}/movie/popular${this.API_KEY}&page=${this.page}&language=en-US`
+      );
+      return await response.json();
 
-//         createMarkUp(results)
+    } catch (error) {
 
-//     } catch (err) {
-//         console.log(err)
-//     }
-// }
+      throw new Error(error.message);
+    }
+  }
+  incrementPage() {
+    this.page += 1;
+  }
+  resetPage() {
+    this.page = 1;
+  }
+}
+
+const movieWrapperEl = document.querySelector('.js-movies-wrapper') 
+
+const getPopularMoviesIns = new getPopularMovies()
+
+movieWrapperEl.innerHTML = ''
+
+const popularMoviesPageRendering = async() => {
+    try {
+        const { results } = await getPopularMoviesIns.fetchPopularMovies()
+        
+        console.log(results)
+
+        createMarkUp(results)
+
+    } catch (err) {
+        console.log(err)
+    }
+}
 
 // popularMoviesPageRendering()
