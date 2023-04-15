73403ad28f9ebf78bceed46251b7d22505c6f61a
OrestKlymko <116631168+OrestKlymko@users.noreply.github.com>
2023-04-15 19:53:01 +0000

    Deploying to gh-pages from @ OrestKlymko/filmoteka-project@91a32dcd2589dbf65b8258e75cf4246790984875 🚀

--- index.67f77c42.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector(".modal__btn-close").addEventListener("click",t),document.addEventListener("keydown",(function(e){"Escape"===e.key&&t()}));var e=document.querySelector(".modal");function t(){document.querySelector(".modal").classList.remove("modal--visible")}e.addEventListener("click",(function(n){n.target===e&&t()}))}();
-//# sourceMappingURL=index.67f77c42.js.map
