ac1d260adef20996b14ab31d22cee91b5125a693
Oksana-Pelekhata <ok.soloviova@gmail.com>
2023-04-15 03:05:13 +0300

    часткова версія рендеру сторінки

---
+++ src/js/popular-movies-page-rendering.js
@@ -0,0 +1,17 @@
+// import { createMarkUp } from './search-movie-page-rendering'
+// import {getPopularMovies} from './get-popular-movies'
+
+
+// const popularMoviesPageRendering = async() => {
+//     try {
+//         const {results} = await getPopularMovies()
+//         console.log(results)
+
+//         createMarkUp(results)
+
+//     } catch (err) {
+//         console.log(err)
+//     }
+// }
+
+// popularMoviesPageRendering()
