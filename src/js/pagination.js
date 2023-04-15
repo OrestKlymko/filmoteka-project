0e91fab88bac9bdee20befd959680fdb2ecea716
Anna Kravets <annakravets1906@gmail.com>
2023-04-15 14:59:43 +0300

    Update pagination.js

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -12,38 +12,6 @@ refs.paginationEl.addEventListener('click', pageButtonNext);
 const getPopularMoviesAPI = new getPopularMovies();
 let currentPage = 1;
 
-function pagination(el) {
-  const container = document.getElementById('tui-pagination-container');
-  const options = {
-    totalItems: `${el.total_results}`,
-    itemsPerPage: `${el.results.length}`,
-    visiblePages: 5,
-    page: 1,
-    centerAlign: false,
-    firstItemClassName: 'tui-first-child',
-    lastItemClassName: 'tui-last-child',
-    template: {
-      page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-      currentPage:
-        '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-      moveButton:
-        '<a href="#" class="tui-page-btn tui-{{type}}">' +
-        '<span class="tui-ico-{{type}}">{{type}}</span>' +
-        '</a>',
-      disabledMoveButton:
-        '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-        '<span class="tui-ico-{{type}}">{{type}}</span>' +
-        '</span>',
-      moreButton:
-        '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-        '<span class="tui-ico-ellip">...</span>' +
-        '</a>',
-    },
-  };
-
-  const pagination = new Pagination(container, options);
-}
-
 getPopularMoviesAPI
   .fetchPopularMovies()
   .then(data => {
@@ -55,7 +23,7 @@ getPopularMoviesAPI
     console.log(error);
   });
 
-function pagination(el) {
+function paginationFunction(el) {
   const container = document.getElementById('tui-pagination-container');
   const options = {
     totalItems: `${el.total_results}`,
