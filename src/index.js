22d7ec92e293eefc9f1670ef66402b52e26b4a27
Orest Klymko <116631168+OrestKlymko@users.noreply.github.com>
2023-04-15 21:24:42 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+<<<<<<< HEAD
 import './js/movie_modal';
+=======
+
+import './movie_modal';
+
+// @import './js/get-movie-by-id.js';
+// @import './js/get-movie-by-name.js'
+// @import './js/get-movie-details.js'
+// @import './js/get-popular-movies.js'
+// @import './js/get-trailer.js'
+// @import './js/local-storage.js'
+// @import './js/pagination.js'
+// @import './js/spinner.js'
+// @import './js/popular-movies-page-rendering.js'
+// @import './js/search-movie-page-rendering'
+
+>>>>>>> main
