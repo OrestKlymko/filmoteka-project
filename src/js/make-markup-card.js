2db6b642bbe8958db96a95f22c43b71024a0302e
Zaslavskyi <bbaly89@gmail.com>
2023-04-17 00:16:44 +0300

    Типу так?

--- src/js/make-markup-card.js
+++ src/js/make-markup-card.js
@@ -1,37 +1,30 @@
-import fetchMoviesByName from "./get-movie-by-name";
 
-export function makeMarkupCard(movies) {
-    return movies
-        .map(({ id, movieName, title, poster_path, release_date}) => {
-            const releaseYear = new Date(release_date);
-            const genres = fetchMoviesByName(movieName);
-            const slicedGenres = sliceGenres(genres);
-            let posterLink = `${urls.imageUrl}${poster_path}`;
-            if (poster_path === null) {
-            posterLink = `$ якйись імедж`;
-        }
-        return `
-                    <div class="card__img-wrap" data-id=${id}>
-                        <img src="${posterLink}" alt="${title}" class="card__img"/>
-                    </div>
-                    <div class="card__text-wrap">
-                        <h2 class="card__name">${title}</h2>
-                        <div class="card__info">
-                            <p class="card__genres">${slicedGenres.join(', ')}</span></p>
-                            <p class="card__year">${releaseYear.getFullYear()}</p>
-                        </div>
-                    </div>`;
-    })
-    .join('');
-}
-
-function sliceGenres(genres) { 
-    
-    if (genres.length < 4) {
+function sliceCategories(categories) { 
+    if (categories.length < 4) {
         
-        return genres
+        return categories
     } else {
-        return [...genres.slice(0, 2), "Other"];
+        return [...categories.slice(0, 2), "Other"];
         
     }
 };
+
+
+export function makeMarkupCard(name, sliceCategories, year) {
+    const card = `
+      <div class="card__img-wrap">
+        <img src="#" alt="" class="card__img" />
+      </div>
+      <div class="card__text-wrap">
+        <h2 class="card__name">${name}</h2>
+        <div class="card__info">
+          <p class="card__genres">${sliceCategories.join(', ')}</p>
+          <p class="card__year">${year}</p>
+        </div>
+      </div>
+    `;
+    return card;
+  }
+
+
+
