5ee11f8c5d5e8a7825b3bc8899b3ba336f7a18b9
Orest Klymko <116631168+OrestKlymko@users.noreply.github.com>
2023-04-16 10:38:22 +0200

    Update pagination.js

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -2,7 +2,7 @@
 
 
 export class Pagination {
-import getPopularMovies from './get-popular-movies';
+import {getPopularMovies} from './get-popular-movies';
 import Pagination from 'tui-pagination';
 
 const paginationEl = document.querySelector('.tui-pagination');
