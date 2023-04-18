570022be4e83ea623ed4e4a778547ee9c840f710
Orest Klymko <116631168+OrestKlymko@users.noreply.github.com>
2023-04-18 23:52:39 +0200

    Update lib.js

--- src/lib.js
+++ src/lib.js
@@ -1,8 +1,8 @@
 // import './js/pagination.js';
 // import './js/get-popular-movies'
-import './js/movie_modal';
-import './js/local-storage';
-import './js/my-library-page-rendering';
-import './js/show-trailer';
-import './js/team';
+import './js/movie_modal.js';
+import './js/local-storage.js';
+import './js/my-library-page-rendering.js';
+import './js/show-trailer.js';
+import './js/team.js';
 // import './js/make-markup-card';
