65a3675b3d9d02736072c425f269a01beb50ab9f
Orest Klymko <116631168+OrestKlymko@users.noreply.github.com>
2023-04-18 22:41:05 +0200

    Update popular-movies-page-rendering.js

--- src/js/popular-movies-page-rendering.js
+++ src/js/popular-movies-page-rendering.js
@@ -1,9 +1,12 @@
-import createMarkUp from './make-markup-card'
-import {getPopularMovies} from './get-popular-movies'
+import createMarkUp from './make-markup-card';
+import { getPopularMovies } from './get-popular-movies';
+import { CustomPagination } from './pagination';
+import { Pagination } from 'tui-pagination';
 
 const movieWrapperEl = document.getElementById('uk') 
 
 const getPopularMoviesIns = new getPopularMovies()
+const paginationConstructor = new CustomPagination();
 
 
 // movieWrapperEl.innerHTML = ''
@@ -13,6 +16,10 @@ const popularMoviesPageRendering = async() => {
         const { results } = await getPopularMoviesIns.fetchPopularMovies()
 
         createMarkUp(results)
+       if (results.length === 0 || !results) {
+      return;
+    }
+      paginationConstructor.paginationFunction(results);
 
     } catch (err) {
         console.log(err)
