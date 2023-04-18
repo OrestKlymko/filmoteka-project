58bc24524a879a108adf1b80974753359b1d54d6
Orest Klymko <116631168+OrestKlymko@users.noreply.github.com>
2023-04-18 22:42:35 +0200

    Update popular-movies-page-rendering.js

--- src/js/popular-movies-page-rendering.js
+++ src/js/popular-movies-page-rendering.js
@@ -3,27 +3,26 @@ import { getPopularMovies } from './get-popular-movies';
 import { CustomPagination } from './pagination';
 import { Pagination } from 'tui-pagination';
 
-const movieWrapperEl = document.getElementById('uk') 
+const movieWrapperEl = document.getElementById('uk');
 
-const getPopularMoviesIns = new getPopularMovies()
+const getPopularMoviesIns = new getPopularMovies();
 const paginationConstructor = new CustomPagination();
-
 
 // movieWrapperEl.innerHTML = ''
 
-const popularMoviesPageRendering = async() => {
-    try {
-        const { results } = await getPopularMoviesIns.fetchPopularMovies()
+const popularMoviesPageRendering = async () => {
+  try {
+    const { results } = await getPopularMoviesIns.fetchPopularMovies();
 
-        createMarkUp(results)
-       if (results.length === 0 || !results) {
+    createMarkUp(results);
+    if (results.length === 0 || !results) {
       return;
     }
-      paginationConstructor.paginationFunction(results);
 
-    } catch (err) {
-        console.log(err)
-    }
-}
+    paginationConstructor.paginationFunction(results);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-popularMoviesPageRendering()
+popularMoviesPageRendering();
