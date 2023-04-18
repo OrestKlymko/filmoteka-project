6b4848de31349a254a125287e67103f292e1ea08
Ihor Karen <karenihor@gmail.com>
2023-04-18 23:00:43 +0300

    open modal in library

--- src/js/movie_modal.js
+++ src/js/movie_modal.js
@@ -18,11 +18,7 @@ const queueBtn = document.querySelector('.queueBtn');
 closeButton.addEventListener('click', closeModal);
 modal.addEventListener('click', handleBackdropClick);
 
-const movieList = document.querySelector('.js-movies-wrapper');
-
-if (!movieList) {
-  return
-}
+const movieList = document.querySelector('.js-movies-wrapper') || document.querySelector('.lib-container');
 
 movieList.addEventListener('click', event => {
   const movieCard = event.target.closest('.carditem');
