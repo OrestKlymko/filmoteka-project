d4096d424c9e08e9cb599cf0206c5d259dc58ef5
Ihor Karen <karenihor@gmail.com>
2023-04-22 20:23:48 +0300

    test 2

--- src/js/authentication.js
+++ src/js/authentication.js
@@ -2,17 +2,20 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import * as firebaseui from 'firebaseui';
 import 'firebaseui/dist/firebaseui.css';
+import 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+const authBtn = document.querySelector('#firebaseui-auth-container')
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAJniPEYyCpEkvhjqmDN6oMVrV91NEI50k",
-  authDomain: "filmoteka-5b686.firebaseapp.com",
-  projectId: "filmoteka-5b686",
-  storageBucket: "filmoteka-5b686.appspot.com",
-  messagingSenderId: "751637418950",
-  appId: "1:751637418950:web:7207f05e032c39a423d8c7",
-  measurementId: "G-W09L62RFSC"
+  apiKey: 'AIzaSyAJniPEYyCpEkvhjqmDN6oMVrV91NEI50k',
+  authDomain: 'filmoteka-5b686.firebaseapp.com',
+  projectId: 'filmoteka-5b686',
+  storageBucket: 'filmoteka-5b686.appspot.com',
+  messagingSenderId: '751637418950',
+  appId: '1:751637418950:web:7207f05e032c39a423d8c7',
+  measurementId: 'G-W09L62RFSC',
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -26,9 +29,7 @@ const ui = new firebaseui.auth.AuthUI(auth);
 // Налаштування FirebaseUI, щоб він використовував авторизацію через Google
 const uiConfig = {
   signInSuccessUrl: '/',
-  signInOptions: [
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID
-  ]
+  signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
 };
 
 // Відображення FirebaseUI на екрані
@@ -38,4 +39,19 @@ ui.start('#firebaseui-auth-container', uiConfig);
 const app = firebase.initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 
-const user = firebase.auth().currentUser;
+const user = firebase.auth().onAuthStateChanged(user => {
+   return user
+});
+// console.log(user);
+
+
+firebase.auth().onAuthStateChanged(user => {   
+  if (user) {
+    // Користувач авторизований
+    authBtn.classList.add('hide')
+    document.querySelector('.menu__link__library').classList.remove('hide');
+  } else {
+    // Користувач не авторизований
+    document.querySelector('.menu__link__library').classList.add('hide');
+  }
+});
--- src/js/local-storage.js
+++ src/js/local-storage.js
@@ -1,12 +1,14 @@
 import getMovieById from './get-movie-by-id';
+import Notiflix from 'notiflix';
+import firebase from 'firebase/compat/app';
+import 'firebase/auth';
 
 const WATCHED_MOVIES = 'watchedMovies';
 const QUEUE_MOVIES = 'queuedMovies';
 
 export let watchedMovies =
   JSON.parse(localStorage.getItem(WATCHED_MOVIES)) || [];
-export let queuedMovies = 
-  JSON.parse(localStorage.getItem(QUEUE_MOVIES)) || [];
+export let queuedMovies = JSON.parse(localStorage.getItem(QUEUE_MOVIES)) || [];
 // import { watchedMovies, queuedMovies } from './local-storage';
 
 const addToWatchedBtn = document.querySelector('.watchedBtn');
@@ -19,33 +21,65 @@ async function handleWatchBtnClick(e) {
   const movieId = e.target.getAttribute('data-movie-id');
   const movieObj = await getMovieById(movieId);
 
-  const isMovieInWatched = watchedMovies.some(
-    movie => movie.id === movieObj.id
-  );
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      const isMovieInWatched = watchedMovies.some(
+        movie => movie.id === movieObj.id
+      );
 
-  if (isMovieInWatched) {
-    watchedMovies = watchedMovies.filter(movie => movie.id !== movieObj.id);
-    addToWatchedBtn.textContent = 'Add to watch';
-    return localStorage.setItem(WATCHED_MOVIES, JSON.stringify(watchedMovies));
-  }
+      if (isMovieInWatched) {
+        watchedMovies = watchedMovies.filter(movie => movie.id !== movieObj.id);
+        addToWatchedBtn.textContent = 'Add to watch';
+        return localStorage.setItem(
+          WATCHED_MOVIES,
+          JSON.stringify(watchedMovies)
+        );
+      }
 
-  watchedMovies.push(movieObj);
-  addToWatchedBtn.textContent = 'Remove from watched';
-  return localStorage.setItem(WATCHED_MOVIES, JSON.stringify(watchedMovies));
+      watchedMovies.push(movieObj);
+      addToWatchedBtn.textContent = 'Remove from watched';
+      return localStorage.setItem(
+        WATCHED_MOVIES,
+        JSON.stringify(watchedMovies)
+      );
+    } else {
+      // Користувач не авторизований
+      Notiflix.Notify.warning(
+        'You need to login to add a movie to your watched'
+      );
+      addToWatchedBtn.textContent = 'Add to watch';
+      return;
+    }
+  });
 }
 
 async function handleQueueBtnClick(e) {
   const movieId = e.target.getAttribute('data-movie-id');
   const movieObj = await getMovieById(movieId);
 
-  const isMovieInQueue = queuedMovies.some(movie => movie.id === movieObj.id);
-  if (isMovieInQueue) {
-    queuedMovies = queuedMovies.filter(movie => movie.id !== movieObj.id);
-    addToQueueBtn.textContent = 'Add to queue';
-    return localStorage.setItem('queuedMovies', JSON.stringify(queuedMovies));
-  }
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      const isMovieInQueue = queuedMovies.some(
+        movie => movie.id === movieObj.id
+      );
+      if (isMovieInQueue) {
+        queuedMovies = queuedMovies.filter(movie => movie.id !== movieObj.id);
+        addToQueueBtn.textContent = 'Add to queue';
+        return localStorage.setItem(
+          'queuedMovies',
+          JSON.stringify(queuedMovies)
+        );
+      }
 
-  queuedMovies.push(movieObj);
-  addToQueueBtn.textContent = 'Remove from queue';
-  return localStorage.setItem('queuedMovies', JSON.stringify(queuedMovies));
+      queuedMovies.push(movieObj);
+      addToQueueBtn.textContent = 'Remove from queue';
+      return localStorage.setItem('queuedMovies', JSON.stringify(queuedMovies));
+    } else {
+      Notiflix.Notify.warning(
+        'You need to login to add a movie to your watched'
+      );
+      addToQueueBtn.textContent = 'Add to queue';
+      return;
+    }
+  });
 }
