73308cc7f90de35a6ff980e12f56df8d002b97b5
Ihor Karen <karenihor@gmail.com>
2023-04-23 20:51:58 +0300

    lib-render 2.0

--- src/js/local-storage.js
+++ src/js/local-storage.js
@@ -1,4 +1,5 @@
 import getMovieById from './get-movie-by-id';
+import { createLibMarkUp } from './lib-render';
 import Notiflix from 'notiflix';
 import firebase from 'firebase/compat/app';
 import 'firebase/auth';
@@ -13,6 +14,7 @@ export let queuedMovies = JSON.parse(localStorage.getItem(QUEUE_MOVIES)) || [];
 
 const addToWatchedBtn = document.querySelector('.watchedBtn');
 const addToQueueBtn = document.querySelector('.queueBtn');
+const movieWrapperEl = document.querySelector('.lib-container');
 
 addToWatchedBtn.addEventListener('click', handleWatchBtnClick);
 addToQueueBtn.addEventListener('click', handleQueueBtnClick);
@@ -30,10 +32,12 @@ async function handleWatchBtnClick(e) {
       if (isMovieInWatched) {
         watchedMovies = watchedMovies.filter(movie => movie.id !== movieObj.id);
         addToWatchedBtn.textContent = 'Add to watch';
-        return localStorage.setItem(
-          WATCHED_MOVIES,
-          JSON.stringify(watchedMovies)
-        );
+        localStorage.setItem(WATCHED_MOVIES, JSON.stringify(watchedMovies));
+
+        if (movieWrapperEl) {
+          createLibMarkUp(watchedMovies);
+        }
+        return;
       }
 
       watchedMovies.push(movieObj);
@@ -65,19 +69,19 @@ async function handleQueueBtnClick(e) {
       if (isMovieInQueue) {
         queuedMovies = queuedMovies.filter(movie => movie.id !== movieObj.id);
         addToQueueBtn.textContent = 'Add to queue';
-        return localStorage.setItem(
-          'queuedMovies',
-          JSON.stringify(queuedMovies)
-        );
+        localStorage.setItem('queuedMovies', JSON.stringify(queuedMovies));
+
+        if (movieWrapperEl) {
+          createLibMarkUp(queuedMovies);
+        }
+        return;
       }
 
       queuedMovies.push(movieObj);
       addToQueueBtn.textContent = 'Remove from queue';
       return localStorage.setItem('queuedMovies', JSON.stringify(queuedMovies));
     } else {
-      Notiflix.Notify.warning(
-        'You need to login to add a movie to your queue'
-      );
+      Notiflix.Notify.warning('You need to login to add a movie to your queue');
       addToQueueBtn.textContent = 'Add to queue';
       return;
     }
